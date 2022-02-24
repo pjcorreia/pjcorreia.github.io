9b28f94334510bb07e5a32c606d89fb5ea6cd4cf
pjcorreia <pjcorreia@gmail.com>
2022-02-24 20:17:43 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        './reg1.html',
+        './index.html',
+        './sw.j',
+        './app.js'
+      ]);
+    })
+  );
+});
